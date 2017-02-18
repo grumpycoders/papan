f9b0e7b295c44bec16cb6719024fbb1e5ab8824c
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2017-02-18 00:36:57 -0800

    Node & Electron work the same way, actually.

---
+++ main-node.js
@@ -0,0 +1,13 @@
+exports.main = () => {
+
+const path = require('path')
+const express = require('express')
+const port = 8080
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, '.')))
+
+app.listen(port)
+
+}
