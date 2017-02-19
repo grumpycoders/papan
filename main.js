318345fb5d05b29faadb956bc9da6d7a0bae41ef
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2017-02-18 22:40:17 -0800

    Adding Polymer dependency, constructing UI a bit, and starting http server all the time under Electron for now.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 if (typeof(process.versions.electron) !== "undefined") {
   const main_electron = require('./main-electron.js')
   main_electron.main()
-} else {
-  const main_node = require('./main-node.js')
-  main_node.main()
 }
+
+const main_node = require('./main-node.js')
+main_node.main()
