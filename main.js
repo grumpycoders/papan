ef71524c8d64b4ac85b4cae5728c829f6051f999
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2017-07-03 22:30:00 +0200

    Starting auth server into vscode.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const PapanUtils = require('./src/common/utils.js')
+const argv = require('minimist')(process.argv.slice(2))
 
 if (PapanUtils.isElectron()) {
   const mainElectron = require('./src/server/main-electron.js')
@@ -9,3 +10,11 @@ if (PapanUtils.isElectron()) {
 
 const mainNode = require('./src/server/main-node.js')
 mainNode.main()
+
+if (argv.auth_server) {
+  let config = {}
+  const http = require('http')
+  const papanAuth = require('./src/server/auth/server.js')
+
+  http.createServer((req, res) => papanAuth.processRequest(req, res, config)).listen(8081)
+}
