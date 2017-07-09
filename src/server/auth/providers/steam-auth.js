67708963fefab921c088003e69cadc86d2dfd42b
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2017-07-08 17:30:46 -0700

    Better registration sequence, so we don't end up with dangling promises.

--- src/server/auth/providers/steam-auth.js
+++ src/server/auth/providers/steam-auth.js
@@ -2,25 +2,27 @@ const SteamStrategy = require('passport-steam').Strategy
 const Provider = require('./provider.js').Provider
 
 exports.register = (passport, users, config) => {
-  const provider = new Provider('steam', 'steam-connect', 'steam')
-  provider.registerStrategies(users, (strategyName, dbCallback) => {
-    passport.use(strategyName, new SteamStrategy(
-      {
-        returnURL: config.httpConfig.baseURL.concat('/auth/steam/callback'),
-        realm: config.httpConfig.baseURL.concat('/'),
-        apiKey: config.steamAuthConfig.apiKey
-      },
-      (identifier, profile, done) => {
-        const user = {
-          provider: 'steam',
-          id: profile.id,
-          screenName: profile.displayName,
-          avatarURL: profile.photos[2].value
+  return new Promise((resolve, reject) => {
+    const provider = new Provider('steam', 'steam-connect', 'steam')
+    provider.registerStrategies(users, (strategyName, dbCallback) => {
+      passport.use(strategyName, new SteamStrategy(
+        {
+          returnURL: config.httpConfig.baseURL.concat('/auth/steam/callback'),
+          realm: config.httpConfig.baseURL.concat('/'),
+          apiKey: config.steamAuthConfig.apiKey
+        },
+        (identifier, profile, done) => {
+          const user = {
+            provider: 'steam',
+            id: profile.id,
+            screenName: profile.displayName,
+            avatarURL: profile.photos[2].value
+          }
+          dbCallback(user).then(user => done(null, user)).catch(err => done(err, false))
         }
-        dbCallback(user).then(user => done(null, user)).catch(err => done(err, false))
-      }
-    ))
-  })
+      ))
+    })
 
-  return Promise.resolve(provider)
+    resolve(provider)
+  })
 }
--- src/server/auth/providers/twitter-auth.js
+++ src/server/auth/providers/twitter-auth.js
@@ -2,25 +2,27 @@ const TwitterStrategy = require('passport-twitter').Strategy
 const Provider = require('./provider.js').Provider
 
 exports.register = (passport, users, config) => {
-  const provider = new Provider('twitter', 'twitter-connect', 'twitter')
-  provider.registerStrategies(users, (strategyName, dbCallback) => {
-    passport.use(strategyName, new TwitterStrategy(
-      {
-        callbackURL: config.httpConfig.baseURL.concat('/auth/twitter/callback'),
-        consumerKey: config.twitterAuthConfig.consumerKey,
-        consumerSecret: config.twitterAuthConfig.consumerSecret
-      },
-      (token, tokenSecret, profile, done) => {
-        const user = {
-          provider: 'twitter',
-          id: profile.id,
-          screenName: profile.displayName,
-          avatarURL: profile.photos[0].value
+  return new Promise((resolve, reject) => {
+    const provider = new Provider('twitter', 'twitter-connect', 'twitter')
+    provider.registerStrategies(users, (strategyName, dbCallback) => {
+      passport.use(strategyName, new TwitterStrategy(
+        {
+          callbackURL: config.httpConfig.baseURL.concat('/auth/twitter/callback'),
+          consumerKey: config.twitterAuthConfig.consumerKey,
+          consumerSecret: config.twitterAuthConfig.consumerSecret
+        },
+        (token, tokenSecret, profile, done) => {
+          const user = {
+            provider: 'twitter',
+            id: profile.id,
+            screenName: profile.displayName,
+            avatarURL: profile.photos[0].value
+          }
+          dbCallback(user).then(user => done(null, user)).catch(err => done(err, false))
         }
-        dbCallback(user).then(user => done(null, user)).catch(err => done(err, false))
-      }
-    ))
-  })
+      ))
+    })
 
-  return Promise.resolve(provider)
+    resolve(provider)
+  })
 }
