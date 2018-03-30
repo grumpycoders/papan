06c275f82f89d9834ad1681b42110ce56b8c96e8
Nicolas Pixel Noble <pixel@nobis-crew.org>
2018-03-29 19:44:25 -0700

    Some cleanup / reorg.

---
+++ src/client/channel.js
@@ -0,0 +1,100 @@
+'use strict'
+
+if (this.PapanUtils.isElectron()) {
+  const electron = require('electron')
+  const ipc = electron.ipcRenderer
+  class Channel {
+    on (event, callback) {
+      ipc.on(event, (event, data) => callback(data))
+    }
+    once (event, callback) {
+      ipc.once(event, (event, data) => callback(data))
+    }
+    send (event, data) {
+      ipc.send(event, data)
+    }
+  }
+  this.channel = new Channel()
+} else {
+  let socket
+
+  class Channel {
+    constructor () {
+      this.listeners = {}
+      socket.on('message', data => {
+        const event = data.event
+        const listeners = this.listeners[event]
+        if (listeners) {
+          let toremove = []
+          for (let index = 0; index < listeners.length; index++) {
+            listeners[index].callback(data.message)
+            if (listeners[index].once) {
+              toremove.unshift(index)
+            }
+          }
+          toremove.forEach(index => {
+            listeners.splice(index, 1)
+          })
+        }
+      })
+    }
+
+    on (event, callback) {
+      if (!this.listeners[event]) {
+        this.listeners[event] = []
+      }
+      this.listeners[event].push({ callback: callback })
+    }
+
+    once (event, callback) {
+      if (!this.listeners[event]) {
+        this.listeners[event] = []
+      }
+      this.listeners[event].push({ callback: callback, once: true })
+    }
+
+    send (event, data) {
+      socket.send({ event: event, message: data })
+    }
+  }
+
+  const iojspath = 'node_modules/socket.io-client/dist/socket.io.js'
+  const iojs = document.createElement('script')
+  iojs.onload = () => {
+    socket = this.io()
+    let channel = new Channel()
+    this.channel.spillover(channel)
+    this.channel = channel
+  }
+  iojs.src = iojspath
+  this.document.head.appendChild(iojs)
+
+  class Queuer {
+    constructor () {
+      this.ons = []
+      this.onces = []
+      this.sends = []
+    }
+
+    on (event, callback) {
+      this.ons.push({ event: event, callback: callback })
+    }
+
+    once (event, callback) {
+      this.onces.push({ event: event, callback: callback })
+    }
+
+    send (event, data) {
+      this.sends.push({ event: event, data: data })
+    }
+
+    spillover (channel) {
+      this.ons.forEach(on => channel.on(on.event, on.callback))
+      this.onces.forEach(once => channel.once(once.event, once.callback))
+      this.sends.forEach(send => channel.send(send.event, send.data))
+      return channel
+    }
+  }
+
+  this.channel = new Queuer()
+}
