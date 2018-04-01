cfd72c168794fb3ff282a43ac72d98fb0f322d92
Nicolas Pixel Noble <pixel@nobis-crew.org>
2018-03-31 20:19:12 -0700

    Better proto loader.

--- src/server/common/proto.js
+++ src/server/common/proto.js
@@ -4,18 +4,24 @@ const grpc = require('grpc')
 const path = require('path')
 const protobufjs = require('protobufjs')
 const recursive = require('recursive-readdir')
-const protosdirs = ['protos'].map(
-  protopath => path.normalize(path.join(__dirname, '..', '..', '..', protopath))
-)
-let allprotos = {}
 
-exports.load = filename => {
+exports.load = (filenames, paths = ['protos']) => {
+  let allprotos = {}
+  let singleResult = false
+  if (typeof filenames === 'string') {
+    singleResult = true
+    filenames = [filenames]
+  }
+
+  const protosdirs = paths.map(
+    protopath => path.normalize(path.join(__dirname, '..', '..', '..', protopath))
+  )
   let root = new protobufjs.Root()
   root.resolvePath = (origin, target) => {
     let foundValue = ''
     Object.keys(allprotos).forEach(base => {
       allprotos[base].forEach(filename => {
-        if (filename.endsWith(target)) foundValue = filename
+        if (filename === target) foundValue = path.join(base, filename)
       })
     })
     return foundValue
@@ -24,11 +30,12 @@ exports.load = filename => {
   return Promise.all(protosdirs.map(path => recursive(path)))
   .then(results => {
     protosdirs.map((key, index) => {
-      allprotos[key] = results[index]
+      allprotos[key] = results[index].map(fullpath => path.relative(key, fullpath))
     })
-    return protobufjs.load(filename, root)
+    return Promise.all(filenames.map(filename => protobufjs.load(filename, root)))
   })
-  .then(proto => {
-    return grpc.loadObject(proto)
+  .then(protos => {
+    const results = protos.map(proto => grpc.loadObject(proto))
+    return singleResult ? results[0] : results
   })
 }
